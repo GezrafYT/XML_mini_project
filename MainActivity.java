e9717b954a6233cdd82ac27933b4fe8cd3a50974
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2024-09-19 00:07:05 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,16 @@
-package com.example.myfirstapp;
+package com.example.test; // replace with your package name
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.WindowInsetsCompat;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Button;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +21,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void popMessage(View view) {
-        //להקפיץ הודעה
-        Toast.makeText(this, "לחצת על הכפתור!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Message Popped 😎", Toast.LENGTH_SHORT).show();
     }
-    
+
     public void changeButtonColor(View view)
     {
-        
+        Button button = (Button) view;
+//        Random rand = new Random();
+//        int R = rand.nextInt(255);
+//        int G = rand.nextInt(255);
+//        int B = rand.nextInt(255);
+//        button.setBackgroundColor(Color.rgb(R, G, B));
+
+        button.setBackgroundColor(Color.RED);
+    }
+
+    public void changeTextColor(View view)
+    {
+        Button button = (Button) view;
+//        Random rand = new Random();
+//        int R = rand.nextInt(255);
+//        int G = rand.nextInt(255);
+//        int B = rand.nextInt(255);
+//        button.setTextColor(Color.rgb(R, G, B));
+
+        button.setTextColor(Color.GREEN);
+    }
+
+    public void changeBGColor(View view)
+    {
+        View layout = findViewById(R.id.mainLayout);
+        Random rand = new Random();
+        int R = rand.nextInt(255);
+        int G = rand.nextInt(255);
+        int B = rand.nextInt(255);
+        layout.setBackgroundColor(Color.rgb(R, G, B));
+    }
+
+
+    public void quitApp(View view)
+    {
+        finishAffinity();
+        System.exit(0);
     }
 }
